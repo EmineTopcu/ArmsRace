44b34ee6aa361220090590a7e94df5b252e8e541
EmineTopcu <50247946+EmineTopcu@users.noreply.github.com>
2021-11-25 14:49:47 -0500

    stats introduced

--- ArmRace.R
+++ ArmRace.R
@@ -3,155 +3,131 @@ library(htmlwidgets)
 library(plotly)
 
 source("Const.R")
+source("RangeFunctions.R")
+
+# escape begavior - by default
+# startle
+# learn from startle
+# duration= 2 time how long it takes for moth to cross diagonally
+# repeat 100 times
+# percentage of capture, time till capture, for both bath and moth
+
+height <- 150
+width <- 150
+timerange <- 50
+dt <- 0.1
+
+
+dangerzone <- 1
+batrange_dist <- 15
+batrange_angle <- 150
+mothrange <- 100
+
+numofbats <- 3
+velbat <- 10 # 3-15
+velbatsd <- 2
+numofmoths <- 10
+velmoth <- 5
+
 
 df_animals <- data.frame(matrix(ncol = 6, nrow = 0))
 colnames(df_animals) <- c('bm', 'seq', 'x', 'y', 'angle', 'vel')
 
+df_lunchtime <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(df_animals) <- c('moth_seq', 'bat_seq', 'eaten_at')
+
+
 trace_df <- data.frame(matrix(ncol = 5, nrow = 0))
 colnames(trace_df) <- c('bm', 'seq', 'time', 'x', 'y') #, 'angle', 'vel')
-masterofmoths <- rep(0, numofmoths)
 
 initializeAnimals <- function(num, vel, bm)
 {
-  y <- runif(num, 1, height)
-  x <- runif(num, 1, width)
-  angle <- runif(num,0, 360)
-  vel <- rep(vel, num)
-  df_animals <<- rbind(df_animals, (as.data.frame(list(bm=rep(bm, num), seq=(1:num), x = x, y = y, angle = angle, vel = vel))))
+    y <- runif(num, 1, height)
+    x <- runif(num, 1, width)
+    angle <- runif(num,0, 360)
+    vel <- rep(vel, num)
+    df_animals <<- rbind(df_animals, (as.data.frame(list(bm=rep(bm, num), seq=(1:num), x = x, y = y, angle = angle, vel = vel))))
 }
 
-initializeAnimals(numofbats, velbat, 'b')
-initializeAnimals(numofmoths, velmoth, 'm')
+initializeAnimals(numofbats, velbat, 'Bat')
+initializeAnimals(numofmoths, velmoth, 'Moth')
 
-nextstep <- function(x, y, angle, vel, dt){
-  deltax <- vel * dt *  cos(angle * pi / 180)
-  deltay <- vel * dt *  sin(angle * pi / 180)
-  turn <- FALSE
-  quartile <- replicate(4 , 1)
-  if (x + deltax < 0)
-  {
-    quartile[2] <- 0
-    quartile[3] <- 0
-  }
-  else if (x + deltax > width)
-  {
-    quartile[1] <- 0
-    quartile[4] <- 0
-  }
-  if (y + deltay < 0)
-  {
-    quartile[3] <- 0
-    quartile[4] <- 0
-  }
-  else if (y + deltay > height)
-  {
-    quartile[1] <- 0
-    quartile[2] <- 0
-  }
-  if (sum(quartile) < 4)
-  {
-    direction <- which(quartile == 1)
-    minangle <- (direction[1] - 1) * 90
-    if (sum(quartile) == 1)
-      maxangle <- minangle + 90
-    else
-      maxangle <- (direction[-1]) * 90
-    if (minangle == 0 & maxangle == 360)
-    { minangle <- -90; maxangle <- 90 }
-    angle<- runif(1, minangle, maxangle) %% 360
-    return (nextstep(x, y, angle, vel, dt))
-  }
-  x <- x + deltax
-  y <- y + deltay
-  return (c(x, y, angle))
-}
-
-
-dist <- function(x1, y1, x2, y2)
+BatCatchesMoth <- function(batseq, mothseq, t)
 {
-  return (sqrt((x1-x2)^2 + (y1-y2)^2))
-}
-
-calcAngle <- function(x1, y1, x2, y2)
-{
-  if (x1 == x2)
-  {
-    if (x2 > x1) 
-      return (0)
-    else 
-      return (180)
-  }
-  alpha <- atan((y2 - y1) / (x2 - x1)) * 180 / pi
-  if (x2 < x1)
-    alpha <- alpha + 180
-  return (alpha)
-}
-
-withinRange <- function(x1, y1, x2, y2, range, alpha, beta)
-{
-  if (dist(x1,y1,x2,y2) > range)
-    return(FALSE)
-  
-  
+    df_lunchtime <<-  rbind(df_lunchtime, (as.data.frame(list(moth_seq=mothseq, bat_seq=batseq, eaten_at = t))))
     
+    df_animals[df_animals$bm=='Moth' & df_animals$seq == mothseq, "x"] <<- -width
+    df_animals[df_animals$bm=='Moth' & df_animals$seq == mothseq, "y"] <<- -height
+    df_animals[df_animals$bm=='Moth' & df_animals$seq == mothseq, "vel"] <<- 0
 }
 
 timeincr <- function(t)
 {
-  for (i in (1:nrow(df_animals)))
-  {
-    if (df_animals$vel[i] > 0)
+    for (i in (1:nrow(df_animals)))
     {
-      ns <- nextstep(df_animals$x[i], df_animals$y[i], df_animals$angle[i], df_animals$vel[i], 1)
-      df_animals$x[i] <<- ns[1]
-      df_animals$y[i] <<- ns[2]
-      df_animals$angle[i] <<- ns[3]
-      trace_df <<- rbind(trace_df, as.data.frame(list(bm=df_animals$bm[i], seq=df_animals$seq[i], t = t, x = ns[1], y = ns[2])))
+        if (df_animals$vel[i] > 0)
+        {
+            ns <- nextstep(df_animals$x[i], df_animals$y[i], df_animals$angle[i], df_animals$vel[i], dt)
+            df_animals$x[i] <<- ns[1]
+            df_animals$y[i] <<- ns[2]
+            df_animals$angle[i] <<- ns[3]
+            trace_df <<- rbind(trace_df, as.data.frame(list(bm=df_animals$bm[i], seq=df_animals$seq[i], t = t, x = ns[1], y = ns[2])))
+        }
+        else
+        {
+            seq <- df_animals$seq[i]
+            masterseq <- df_lunchtime[df_lunchtime$moth_seq == seq, 'bat_seq']
+            masterpos <- df_animals[df_animals$bm == 'Bat' & df_animals$seq == masterseq,]
+            trace_df <<- rbind(trace_df, as.data.frame(list(bm=df_animals$bm[i], seq=df_animals$seq[i], t = t, x = masterpos$x, y = masterpos$y))) #, angle = masterpos$angle , vel = masterpos$vel))
+        }
     }
-    else
+    moth_df <- df_animals[df_animals$bm == 'Moth', ]
+    bat_df <- df_animals[df_animals$bm == 'Bat',]
+    for (i in (1:nrow(moth_df)))
     {
-      seq <- df_animals$seq[i]
-      masterseq <- masterofmoths[seq]
-      masterpos <- df_animals[df_animals$bm == 'b' & df_animals$seq == masterseq,]
-      trace_df <<- rbind(trace_df, as.data.frame(list(bm=df_animals$bm[i], seq=df_animals$seq[i], t = t, x = masterpos$x, y = masterpos$y))) #, angle = masterpos$angle , vel = masterpos$vel))
+        mothx <-  moth_df$x[i]
+        mothy <-  moth_df$y[i]
+        mothseq <- moth_df$seq[i]
+        for (j in (1:nrow(bat_df)))
+        {
+            batx <-  bat_df$x[j]
+            baty <-  bat_df$y[j]
+            batseq <- bat_df$seq[j]
+            batangle <- bat_df$angle[j]
+            moth_detected <- withinRange(batx, baty, mothx, mothy, batrange_dist, batangle, batrange_angle)
+            if (moth_detected) # bat detects moths, turns towards
+            {
+                alpha <- calcAngle(batx, baty, mothx, mothy)
+                df_animals[df_animals$bm=='Bat' & df_animals$seq == batseq, "angle"] <<- alpha
+            }
+            dist <- dist(mothx, mothy, batx, baty)
+            if (dist < dangerzone)
+            {
+                BatCatchesMoth (bat_df$seq[j], mothseq, t) 
+            }
+            else if (dist < mothrange)
+            {
+                alpha <- calcAngle(batx, baty, mothx, mothy)
+                df_animals[df_animals$bm=='Moth' & df_animals$seq == mothseq, "angle"] <<- alpha
+            }
+        }  
     }
-  }
-  moth_df <- df_animals[df_animals$bm == 'm', ]
-  bat_df <- df_animals[df_animals$bm == 'b',]
-  for (i in (1:nrow(moth_df)))
-  {
-    mothx <-  moth_df$x[i]
-    mothy <-  moth_df$y[i]
-    mothseq <- moth_df$seq[i]
-    for (j in (1:nrow(bat_df)))
-    {
-      batx <-  bat_df$x[j]
-      baty <-  bat_df$y[j]
-      batseq <- bat_df$seq[j]
-      dist <- dist(mothx, mothy, batx, baty)
-      if (dist < batrange) # bat detects moths, turns towards
-      {
-        alpha <- calcAngle(batx, baty, mothx, mothy)
-        df_animals[df_animals$bm=='b' & df_animals$seq == batseq, "angle"] <<- alpha
-      }
-      if (dist < dangerzone)
-      {
-        masterofmoths[mothseq] <<-  bat_df$seq[j]
-        df_animals[df_animals$bm=='m' & df_animals$seq == mothseq, "x"] <<- -width
-        df_animals[df_animals$bm=='m' & df_animals$seq == mothseq, "y"] <<- -height
-        df_animals[df_animals$bm=='m' & df_animals$seq == mothseq, "vel"] <<- 0
-      }
-    }  
-  }
 }
 
-
-for (i in (1:100))
+Simulate <- function()
 {
-  timeincr(i)
+    stats <- c('Victim' = 0, 'Prey' = 0, 'HuntTime' = 0)
+    for (i in seq(1, timerange, dt))
+    {
+        timeincr(i)
+    }
+    stats["Victim"] <- nrow(df_lunchtime) / numofmoths
+    stats["Prey"] <- nrow(df_lunchtime) / numofbats
+    stats["HuntTime"] <- mean(df_lunchtime$eaten_at)
+    return (stats)
 }
 
-
+stats <- Simulate()
 
 p <- ggplot(trace_df, aes(x, y, frame = t)) +
  xlim(0, width) + ylim(0, height) + theme_bw() +
@@ -164,4 +140,11 @@ p <- ggplot(trace_df, aes(x, y, frame = t)) +
 
 pp <- ggplotly(p) %>% animation_opts(500, easing = "linear", redraw = FALSE)
 pp
-saveWidget(pp, file = "animation.html", selfcontained = TRUE)
+# saveWidget(pp, file = "animation.html", selfcontained = TRUE)
+
+dd <- function(a)
+{
+    return (a**2)
+}
+
+dd(c(5,7))   
