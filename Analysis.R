57842b7c19b552256d0a6963700192fd120f46f6
EmineTopcu <50247946+EmineTopcu@users.noreply.github.com>
2021-11-28 22:36:33 -0500

    Analysis.R Need to find a way to make aes(x) parametric

---
+++ Analysis.R
@@ -0,0 +1,60 @@
+library(ggplot2)
+library(tidyverse)
+
+DF.Analysis <- read.csv("Output/StatsBatRange.csv")
+
+facetLabel <- function(string) {
+    if (gmode == "")
+        return ("")
+    paste(gmode, string)
+}
+
+plotParam <- function(mode, label)
+{
+    gmode <<- label
+    DF.Sub <- DF.Analysis %>%
+    filter(startsWith(DF.Analysis$Mode, mode)) 
+    
+    pVictim <- DF.Sub %>% 
+         ggplot( aes(x=Victim, color=Mode, fill=Mode)) +
+         geom_histogram(alpha=0.6) +
+         theme(
+             legend.position="none",
+             strip.text.x = element_text(size = 12)
+         ) +
+         xlab("") +
+         ylab("Victim %") +
+        facet_wrap(~Variable, labeller = labeller( Variable = facetLabel))
+    ggsave(filename = paste(mode, "Victim.jpg"), plot = pVictim)
+     
+     pPrey <- DF.Sub %>%
+         ggplot( aes(x=Prey, color=Mode, fill=Mode)) +
+         geom_histogram(alpha=0.6) +
+         theme(
+             legend.position="none",
+             strip.text.x = element_text(size = 12)
+         ) +
+         xlab("") +
+         ylab("Prey Count") +
+         facet_wrap(~Variable, labeller = labeller( Variable = facetLabel))
+     ggsave(filename = paste(mode, "Prey.jpg"), plot = pPrey)
+
+    pHunt <- DF.Sub %>%
+        ggplot( aes(x=HuntTime, color=Mode, fill=Mode)) +
+        geom_histogram(alpha=0.6) +
+        theme(
+            legend.position="none",
+            strip.text.x = element_text(size = 12)
+        ) +
+        xlab("") +
+        ylab("HuntTime") +
+        facet_wrap(~Variable, labeller = labeller( Variable = facetLabel))
+    ggsave(filename = paste(mode, "Hunt.jpg"), plot = pHunt)
+    pHunt
+    
+    
+}
+
+
+plotParam("RandomHunt", "")
+plotParam("BatRangeDistance", "Distance: ")
